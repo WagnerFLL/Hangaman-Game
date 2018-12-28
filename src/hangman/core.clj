bb7073386288f0a070bc67025193d36a24f0b91e
WagnerFLL <wsf@ic.ufal.br>
2018-12-28 15:48:10 -0300

    Improve game :sparkles:

--- src/hangman/core.clj
+++ src/hangman/core.clj
@@ -12,20 +12,55 @@
 (defn hit-whole-word? [word choices]
   (empty? (remove-matches word choices)))
 
+(defn get-let! [] (read-line))
+
+(defn is-contained? [word lett] (.contains word lett))
+
+(defn show-state [lives word choices]
+  (println "Lives: " lives)
+  (doseq [lett (seq word)]
+    (if (contains? choices (str lett))
+      (print lett " ")
+      (print "? ")))
+  (println "")
+  (println ""))
+
 (defn play [lives word choices]
-  (if (= lives 0)
-    (lost)
-    (if (hit-whole-word? word choices)
+  (show-state lives word choices)
+  (cond 
+    (= lives 0) (lost)
+    
+    (hit-whole-word? word choices)
       (print "You win!")
-      (print "Choose a let: "))))
+    
+    :else
+    (let [lett (get-let!)]
+      (if (is-contained? word lett)
+        (do
+          (println "Ok!")
+          (recur lives word (conj choices lett)))
+
+        (do
+          (println "-1 life")
+          (recur (dec lives) word choices))))))
 
 (defn fib[x]
     (if (= x 0) 0 
     (if (= x 1) 1 
     (+ (fib (- x 1)) (fib (- x 2))))))
 
+(defn sum [x y]
+  (if (= x 1)
+    (inc y)
+    (recur (dec x) (+ y x))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn no-interval [x]
+  (filter 
+    (fn [e] 
+      (or (< e 2) (> e 4))) 
+    x))
